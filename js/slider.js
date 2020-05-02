011e88e38b22a89e8cf177d48780c1313fc7ff64
Neodaniela <daniela.pikur@gmail.com>
2020-05-02 23:54:21 +0300

    Выполнено подключение и стилизация слайдера, доработана вёрстка формы и футера на десктопной версии

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    slidesToShow: 3,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    centerMode: true,
+  });
+});
